2f6d56415a177897ad3c5678896ae367f6200f58
tlrhdrks1275 <tlrhdrks12@naver.com>
2026-08-17 14:58:47 +0900

    Add files via upload

--- 2/2-1.c
+++ 2/2-1.c
@@ -1,1 +1,48 @@
+/******************************************************************************
 
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void swap1(int a, int b)
+{
+    int tmp;
+    tmp=a;
+    a=b;
+    b=tmp;
+    printf("After value swap(in function), A : %d, B : %d\n", a, b);
+}
+
+void swap2(int *a, int *b)
+{
+    int tmp;
+    tmp=*a;
+    *a=*b;
+    *b=tmp;
+    printf("After value swap(in function), A : %d, B : %d\n", *a, *b);
+}
+
+
+int main()
+{
+    int a, b;
+    printf("A: ");
+    scanf("%d", &a);
+    
+    printf("B: ");
+    scanf("%d", &b);
+    
+    printf("Before swap, A : %d, B : %d\n\n", a, b);
+    
+    swap1(a, b);
+    printf("After value swap(in main), A : %d, B : %d\n\n", a, b);
+    
+    swap2(&a, &b);
+    printf("After value swap(in main), A : %d, B : %d", a, b);
+    
+    return 0;
+}
